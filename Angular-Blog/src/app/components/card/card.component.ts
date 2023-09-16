3f8f6bae1f53f027e75918c432c1190cdf23204a
Bruno Alcantara <120646838+BrunoAlcantaraGit@users.noreply.github.com>
2023-09-15 22:51:25 -0300

    Tornando o componente dinamido
    conteúdo substituído por variável interpolada no html

--- Angular-Blog/src/app/components/card/card.component.ts
+++ Angular-Blog/src/app/components/card/card.component.ts
@@ -9,4 +9,5 @@ export class LeftCardComponent {
 
 textoPrincipal: string= 'AWS Permission Manage Reimagined'
 buttton: string = 'Book a Demo'
+conteudo:string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nobis suscipit vel quae, possimus distinctio atque vero reprehenderit'
 }
